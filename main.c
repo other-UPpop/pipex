0a7324cf1b61fdedcac80ea4aa74ba9874eaa6ef
rohta <user@rohtapc.local>
2025-05-20 17:20:04 +0900

    20250520_var2.0

--- main.c
+++ main.c
@@ -6,23 +6,41 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 01:04:50 by rohta             #+#    #+#             */
-/*   Updated: 2025/05/20 15:13:01 by rohta            ###   ########.fr       */
+/*   Updated: 2025/05/20 17:19:47 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static int	ok_access(t_arg arg)
+{
+	if (access(arg.c_arg[3], (F_OK)) == 0)
+		return (0);
+	return (1);
+}
+
+static void	wait_free(t_pid pid, t_arg arg)
+{
+	waitpid(pid.p1, NULL, 0);
+	waitpid(pid.p2, NULL, 0);
+	free_args(arg.c_arg);
+	free_args(arg.s_arg);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_arg	arg;
 	t_fd	fd;
 	t_pid	pid;
+	int		open;
 	int		pipefd[2];
 
+	open = 0;
 	if (argc < 5)
 		return (1);
 	get_arg_ck(argc, argv, &arg);
 	fd.get_fd = get_file_open(arg);
+	open = ok_access(arg);
 	fd.out_fd = out_file_open(arg, fd);
 	if (pipe_init(pipefd) == 1)
 		return (-1);
@@ -33,9 +51,6 @@ int	main(int argc, char **argv, char **envp)
 	if (pid.p2 == 0)
 		pid2_cmd(arg, fd, pipefd, envp);
 	parent_process_close(pipefd, fd);
-	waitpid(pid.p1, NULL, 0);
-	waitpid(pid.p2, NULL, 0);
-	free_args(arg.c_arg);
-	free_args(arg.s_arg);
-	return (0);
+	wait_free(pid, arg);
+	return (open);
 }
