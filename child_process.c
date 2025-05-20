aafe0de9837caf3e47455b0592f2cfa895268470
rohta <user@rohtapc.local>
2025-05-20 18:08:05 +0900

    20250520_var2.4

--- child_process.c
+++ child_process.c
@@ -6,7 +6,7 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 20:17:04 by rohta             #+#    #+#             */
-/*   Updated: 2025/05/20 15:10:20 by rohta            ###   ########.fr       */
+/*   Updated: 2025/05/20 18:07:48 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	error_print(char *arg)
 	write(STDERR_FILENO, e_str2, ft_strlen(e_str2));
 }
 
-static void	execve_cmd(char *cmd, char **envp)
+static void	execve_cmd(char *cmd, char **envp, t_arg arg)
 {
 	char	**args;
 	char	*cmd_path;
@@ -33,7 +33,8 @@ static void	execve_cmd(char *cmd, char **envp)
 	cmd_path = get_cmd_path(args[0], envp);
 	if (!cmd_path)
 	{
-		error_print(args[0]);
+		if (access(arg.c_arg[3], (W_OK | F_OK)) != 0)
+			error_print(args[0]);
 		free_args(args);
 		exit (1);
 	}
@@ -51,7 +52,7 @@ void	pid1_cmd(t_arg arg, t_fd fd, int pipefd[2], char **envp)
 	close(pipefd[1]);
 	close(fd.get_fd);
 	close(fd.out_fd);
-	execve_cmd(arg.c_arg[1], envp);
+	execve_cmd(arg.c_arg[1], envp, arg);
 	free_args(arg.c_arg);
 	free_args(arg.s_arg);
 	exit(1);
@@ -65,7 +66,7 @@ void	pid2_cmd(t_arg arg, t_fd fd, int pipefd[2], char **envp)
 	close(pipefd[1]);
 	close(fd.get_fd);
 	close(fd.out_fd);
-	execve_cmd(arg.c_arg[2], envp);
+	execve_cmd(arg.c_arg[2], envp, arg);
 	free_args(arg.c_arg);
 	free_args(arg.s_arg);
 	exit(1);
