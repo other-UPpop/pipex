641eaeed3ce8e169c954d1d59757d626fef3cc73
rohta <user@rohtapc.local>
2025-05-20 17:30:11 +0900

    20250520_var2.1

--- file_open.c
+++ file_open.c
@@ -6,13 +6,25 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 20:11:09 by rohta             #+#    #+#             */
-/*   Updated: 2025/05/20 17:13:31 by rohta            ###   ########.fr       */
+/*   Updated: 2025/05/20 17:29:51 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	error_print_file(t_arg arg)
+static void	error_print_open2_file(t_arg arg)
+{
+	char	*e_str1;
+	char	*e_str2;
+
+	e_str1 = "bash: ";
+	e_str2 = ": Permission denied\n";
+	write(STDERR_FILENO, e_str1, ft_strlen(e_str1));
+	write(STDERR_FILENO, arg.c_arg[3], ft_strlen(arg.c_arg[3]));
+	write(STDERR_FILENO, e_str2, ft_strlen(e_str2));
+}
+
+static void	error_print_open_file(t_arg arg)
 {
 	char	*e_str1;
 	char	*e_str2;
@@ -31,7 +43,7 @@ int	get_file_open(t_arg arg)
 	get_file = open(arg.c_arg[0], O_RDONLY);
 	if (get_file < 0)
 	{
-		error_print_file(arg);
+		error_print_open_file(arg);
 		free_args(arg.c_arg);
 		free_args(arg.s_arg);
 		exit (0);
@@ -46,7 +58,7 @@ int	out_file_open(t_arg arg, t_fd fd)
 	get_file = open(arg.c_arg[3], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (get_file < 0)
 	{
-		perror("outfile");
+		error_print_open2_file(arg);
 		close(fd.get_fd);
 		free_args(arg.c_arg);
 		free_args(arg.s_arg);
