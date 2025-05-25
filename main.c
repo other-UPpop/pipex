b89100b505563148bde318beb466e5d8d75b70be
rohta <user@rohtapc.local>
2025-05-25 23:37:07 +0900

    20250525_var1.0.4

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 20:59:47 by rohta             #+#    #+#             */
-/*   Updated: 2025/05/25 22:50:29 by rohta            ###   ########.fr       */
+/*   Updated: 2025/05/25 23:36:05 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,14 @@ static void	init_pipex(t_arg *arg, t_pidfd *pidfd, t_error *err)
 
 int	main(int ac, char **av, char **envp)
 {
-	t_arg	*arg;
-	t_pidfd	*pidfd;
-	t_error	*err;
+	t_arg	arg;
+	t_pidfd	pidfd;
+	t_error	err;
 
-	arg = NULL;
-	pidfd = NULL;
-	err = NULL;
-	init_pipex(arg, pidfd, err);
-	arg_check(ac, av, err);
-	get_arg(ac, av, arg);
-	file_open(arg, pidfd);
-	pipex(arg, pidfd, err, envp);
-	return (err->return_num);
+	init_pipex(&arg, &pidfd, &err);
+	arg_check(ac, av, &err);
+	get_arg(ac, av, &arg);
+	file_open(&arg, &pidfd);
+	pipex(&arg, &pidfd, &err, envp);
+	return (err.return_num);
 }
