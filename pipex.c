12a21f050b88804ca3230905e30038bcd6d1c4f3
rohta <user@rohtapc.local>
2025-05-26 01:55:35 +0900

    20250526_var1.2.2

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 19:28:31 by rohta             #+#    #+#             */
-/*   Updated: 2025/05/26 01:36:13 by rohta            ###   ########.fr       */
+/*   Updated: 2025/05/26 01:55:26 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static void	parent_close(t_pidfd *pidfd)
 	close(pidfd->out_fd);
 }
 
-static void	wait_free(t_arg *arg, t_pidfd *pidfd)
+static void	wait_free(t_arg *arg, t_pidfd *pidfd, t_error *err)
 {
 	int	status1;
 	int	status2;
@@ -40,6 +40,12 @@ static void	wait_free(t_arg *arg, t_pidfd *pidfd)
 	status2 = 0;
 	waitpid(pidfd->p1, &status1, 0);
 	waitpid(pidfd->p2, &status2, 0);
+	if (status1 == 0 && status2 == 0)
+		err->return_num = 0;
+	else if (status2 == 256)
+		err->return_num = 1;
+	else if (status2 == 32512)
+		err->return_num = 127;		
 	free_args(arg);
 }
 
@@ -49,5 +55,5 @@ void	pipex(t_arg *arg, t_pidfd *pidfd, t_error *err, char **envp)
 		return ;
 	do_cmd(arg, pidfd, err, envp);
 	parent_close(pidfd);
-	wait_free(arg, pidfd);
+	wait_free(arg, pidfd, err);
 }
