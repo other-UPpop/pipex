24c7bb52b0081c1dfceabc0017e58f804f517d64
rohta <user@rohtapc.local>
2025-05-26 01:48:29 +0900

    20250526_var1.2.1

--- do_command.c
+++ do_command.c
@@ -6,7 +6,7 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 20:25:26 by rohta             #+#    #+#             */
-/*   Updated: 2025/05/26 00:33:17 by rohta            ###   ########.fr       */
+/*   Updated: 2025/05/26 01:48:18 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,7 @@ static void	execve_cmd2(char *cmd, t_arg *arg, t_error *err, char **envp)
 			error_print_cmd(args[0], err);
 		free_arg(args);
 		free_args(arg);
-		err->return_num = 127;
-		exit(err->return_num);
+		exit(127);
 	}
 	execve(cmd_path, args, envp);
 	if (err->log < 2)
